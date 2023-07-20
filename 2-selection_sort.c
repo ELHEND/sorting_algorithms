78479e9e7e8d883f0843191b6dab185dba317f4a
ELHEND <alhnd683@gmail.com>
2023-07-20 04:34:03 -0700

    rfg

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,10 +6,10 @@
  *     */
 void swap_ints(int *a, int *b)
 {
-	        int tmp;
-		        tmp = *a;
-			        *a = *b;
-				        *b = tmp;
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 /**
  *  *  * selection_sort - Sort an array of integers in ascending order
@@ -21,19 +21,19 @@ void swap_ints(int *a, int *b)
  *        */
 void selection_sort(int *array, size_t size)
 {
-	        int *min;
-		        size_t i, k;
-			        if (array == NULL || size < 2)
-					                return;
-				        for (i = 0; i < size - 1; i++)
-						        {
-								                min = array + i;
-										                for (k = i + 1; k < size; k++)
-													                        min = (array[k] < *min) ? (array + k) : min;
-												                if ((array + i) != min)
-															                {
-																		                        swap_ints(array + i, min);
-																					                        print_array(array, size);
-																								                }
-														        }
+	int *min;
+	size_t i, k;
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (k = i + 1; k < size; k++)
+			min = (array[k] < *min) ? (array + k) : min;
+		if ((array + i) != min)
+		{
+			swap_ints(array + i, min);
+			print_array(array, size);
+		}
+	}
 }
