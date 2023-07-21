f7a12bb6c2d7463251910cdec070228aead940f3
ELHEND <alhnd683@gmail.com>
2023-07-21 06:57:10 -0700

    wwwwwww

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,16 +2,17 @@
 
 /**
  *  * swap_ints - Swap two integers in an array.
- *   * @a: The first integer to swap.
- *    * @b: The second integer to swap.
- *     */
+ *   * @a:  first integer to swap.
+ *    * @b: second integer to swap.
+ */
+
 void swap_ints(int *a, int *b)
 {
-		int tmp;
+	int tmp;
 
-			tmp = *a;
-				*a = *b;
-					*b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -20,27 +21,28 @@ void swap_ints(int *a, int *b)
  *    * @size: The size of the array.
  *     *
  *      * Description: Prints the array after each swap.
- *       */
+ */
+
 void bubble_sort(int *array, size_t size)
 {
-		size_t i, len = size;
-			bool bubbly = false;
+	size_t m, len = size;
+	bool bubbly = false;
 
-				if (array == NULL || size < 2)
-							return;
+	if (array == NULL || size < 2)
+		return;
 
-					while (bubbly == false)
-							{
-										bubbly = true;
-												for (i = 0; i < len - 1; i++)
-															{
-																			if (array[i] > array[i + 1])
-																							{
-																												swap_ints(array + i, array + i + 1);
-																																print_array(array, size);
-																																				bubbly = false;
-																																							}
-																					}
-														len--;
-															}
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (m = 0; m < len - 1; m++)
+		{
+			if (array[m] > array[m + 1])
+			{
+				swap_ints(array + m, array + m + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
 }
