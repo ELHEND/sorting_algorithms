bca1222be075d1f607a5d89929b1f3c470cb2dcb
ELHEND <alhnd683@gmail.com>
2023-07-21 05:59:01 -0700

    ﷼

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,14 +1,16 @@
 #include "sort.h"
 
 void swap_ints(int *a, int *b);
-void max_heapify(int *array, size_t size, size_t base, size_t root);
 void heap_sort(int *array, size_t size);
+void max_heapify(int *array, size_t size, size_t base, size_t root);
+
 
 /**
  *  * swap_ints - Swap two integers in an array.
- *   * @a: The first integer to swap.
  *    * @b: The second integer to swap.
- *     */
+ *   * @a: The first integer to swap.
+ */
+
 void swap_ints(int *a, int *b)
 {
 	int tmp;
@@ -21,10 +23,11 @@ void swap_ints(int *a, int *b)
 /**
  *  * max_heapify - Turn a binary tree into a complete binary heap.
  *   * @array: An array of integers representing a binary tree.
- *    * @size: The size of the array/tree.
- *     * @base: The index of the base row of the tree.
- *      * @root: The root node of the binary tree.
- *       */
+ *    * @size: size of the array/tree.
+ *     * @base: index of the base row of the tree.
+ *      * @root:  root node of the binary tree.
+ */
+
 void max_heapify(int *array, size_t size, size_t base, size_t root)
 {
 	size_t left, right, large;
@@ -51,24 +54,24 @@ void max_heapify(int *array, size_t size, size_t base, size_t root)
  *   *             order using the heap sort algorithm.
  *    * @array: An array of integers.
  *     * @size: The size of the array.
- *      *
- *       * Description: Implements the sift-down heap sort
+ *     * Description: Implements the sift-down heap sort
  *        * algorithm. Prints the array after each swap.
- *         */
+ */
+
 void heap_sort(int *array, size_t size)
 {
-	int i;
+	int m;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = (size / 2) - 1; i >= 0; i--)
-		max_heapify(array, size, size, i);
+	for (m = (size / 2) - 1; m >= 0; m--)
+		max_heapify(array, size, size, m);
 
-	for (i = size - 1; i > 0; i--)
+	for (m = size - 1; m > 0; m--)
 	{
-		swap_ints(array, array + i);
+		swap_ints(array, array + m);
 		print_array(array, size);
-		max_heapify(array, size, i, 0);
+		max_heapify(array, size, m, 0);
 	}
 }
