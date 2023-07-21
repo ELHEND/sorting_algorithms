5c5853a5375a9f0eb46c19bf2a4ea453234067c6
ELHEND <alhnd683@gmail.com>
2023-07-21 06:34:18 -0700

    srsr

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+
 void cocktail_sort_list(listint_t **list);
 void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
@@ -9,7 +10,8 @@ void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
  *   *                   list of integers with the node ahead of it.
  *    * @list: A pointer to the head of a doubly-linked list of integers.
  *     * @tail: A pointer to the tail of the doubly-linked list.
- *      * @shaker: A pointer to the current swapping node of the cocktail shaker algo.
+ *      * @shaker: A pointer to the current
+ *      *swapping node of the cocktail shaker algo.
  */
 
 void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
@@ -36,7 +38,8 @@ void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
  *   *                    list of integers with the node behind it.
  *    * @list:  pointer to the head of a doubly-linked list of integers.
  *     * @tail:  pointer to the tail of the doubly-linked list.
- *      * @shaker: A pointer to the current swapping node of the cocktail shaker algo.
+ *      * @shaker: A pointer to the current
+ *      swapping node of the cocktail shaker algo.
  */
 
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
@@ -60,8 +63,9 @@ void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
 
 /**
  *  * cocktail_sort_list - Sort a listint_t doubly-linked list of integers in
- *   *                      ascending order using the cocktail shaker algorithm.
- *    * @list: A pointer to the head of a listint_t doubly-linked list.
+ *   *ascending order using the cocktail shaker algorithm.
+ *    * @list: A pointer to the head of
+ *    a listint_t doubly-linked list.
  */
 
 void cocktail_sort_list(listint_t **list)
