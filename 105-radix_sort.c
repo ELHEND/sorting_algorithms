7034771477cf7a49bb332d13a622b8b6aaaee074
ELHEND <alhnd683@gmail.com>
2023-07-23 12:00:50 -0700

    hend

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -63,8 +63,7 @@ void radix_counting_sort(int *array, size_t size, int sig, int *buff)
  *    * @array: An array of integers.
  *     * @size: The size of the array.
  *
- *      *
- *       * Description: Implements the LSD radix sort algorithm. Prints
+ *     * Description: Implements the LSD radix sort algorithm. Prints
  *        * the array after each significant digit increase.
  */
 
