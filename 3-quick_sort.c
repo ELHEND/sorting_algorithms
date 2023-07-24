67e63b5bad8efa3e06eb263897632d5376ee38cd
ELHEND <alhnd683@gmail.com>
2023-07-24 09:13:29 -0700

    t

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -68,6 +68,7 @@ int lomuto_partition(int *array, size_t size, int left, int right)
  *
  *     * @left: starting index of the array partition to order.
  *      * @right: ending index of the array partition to order.
+ *      
  *       * Description: Uses  Lomuto partition scheme.
  */
 
